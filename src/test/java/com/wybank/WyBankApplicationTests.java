8f759dd0f2f39c42248ded6769d4518d221e620b
desktop <viostarryy@gmail.com>
2024-07-06 21:40:53 +0900

    #2 - 스프링 부트 프로젝트 초기화
    라이브러리 : devtools, web, lombok, actuator

---
+++ src/test/java/com/wybank/WyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
