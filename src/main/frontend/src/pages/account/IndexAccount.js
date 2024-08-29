c8174d36039a712d62a259bc525de83e5f62b358
yuri <viostarryy@gmail.com>
2024-08-29 18:05:33 +0900

    .
    .

--- src/main/frontend/src/pages/account/IndexAccount.js
+++ src/main/frontend/src/pages/account/IndexAccount.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import BasicLayout from "../../layouts/BasicLayout";
-import {Link, Outlet} from "react-router-dom";
+import {Link, Outlet, useNavigate} from "react-router-dom";
 
 function IndexAccount(props) {
+
+    const navigate = useNavigate();
+    const handleClickList = useCallback(() => {
+        navigate({pathname: 'list'})
+    }, [])
+    const handleClickCreate = useCallback(() => {
+        navigate({pathname: 'create'})
+    }, [])
+
     return (
         <BasicLayout>
             <div className={"w-full flex m-2 p-2"}>
-                <div className={"m-1 p-2 w-30 font-bold text-center"}><Link to={'list'}>계좌 조회</Link></div>
-                <div className={"m-1 p-2 w-30 font-bold text-center"}><Link to={'create'}>계좌 생성</Link></div>
-                <div className={"m-1 p-2 w-30 font-bold text-center"}><Link to={'info/1'}>1번 계좌</Link></div>
+                <div className={"m-1 p-2 w-30 font-bold text-center"} onClick={handleClickList}>계좌조회</div>
+                <div className={"m-1 p-2 w-30 font-bold text-center"} onClick={handleClickCreate}>계좌생성</div>
+                <div className={"m-1 p-2 w-30 font-bold text-center"}><Link to={'info/1'}>1번계좌</Link></div>
             </div>
             <div className={"flex flex-wrap w-full"}>
                 <Outlet/>
