f12546b9da175137a8adc5811ae31beb67acb42b
yuri <viostarryy@gmail.com>
2024-08-30 12:20:15 +0900

    set: RDS-AWS-SSH
    뭔짓을해도 안됨 그냥 터미널에서 수동으로 터널링 해서 실행누르기

---
+++ src/main/java/com/wybank/config/SshDataSourceConfig.java
@@ -0,0 +1,38 @@
+//package com.wybank.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.context.annotation.Profile;
+//
+//import javax.sql.DataSource;
+//
+//
+//@Slf4j
+//@Profile("local")
+//@Configuration
+//@RequiredArgsConstructor
+//public class SshDataSourceConfig {
+//
+//    private final SshTunnelingInitializer initializer;
+//
+//    @Bean("dataSource")
+//    @Primary
+//    public DataSource dataSource(DataSourceProperties properties) {
+//
+//        Integer forwardedPort = initializer.buildSshConnection();  // ssh 연결 및 터널링 설정
+//        String url = properties.getUrl().replace("[forwardedPort]", Integer.toString(forwardedPort));
+//        log.info("이거어떻게봐??"+url);
+//        return DataSourceBuilder.create()
+//                .url(url)
+//                .username(properties.getUsername())
+//                .password(properties.getPassword())
+//                .driverClassName(properties.getDriverClassName())
+//                .build();
+//    }
+//
+//}
