758e36b2130aaaad96a50e11e72285fe192c4518
dahee <dltmfekgml@naver.com>
2024-09-02 17:01:50 +0900

    feature: 대출상담 조회기능 구현
    counsel Entity에서 -> @Where(clause = "is_deleted=false") 해당 옵션때문에 조회가 제대로 안되는 문제가 있음 , DB는 h2로 진행중 -> 왜그러는지?

--- src/test/java/com/wybank/loan/service/CounselServiceTest.java
+++ src/test/java/com/wybank/loan/service/CounselServiceTest.java
@@ -6,11 +6,16 @@ import com.wybank.loan.counsel.domain.Counsel;
 import com.wybank.loan.counsel.dto.CounselDTO;
 import com.wybank.loan.counsel.repository.CounselRepository;
 import com.wybank.loan.counsel.service.CounselServiceImpl;
+import com.wybank.loan.exception.BaseException;
+import com.wybank.loan.exception.ResultType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class CounselServiceTest {
@@ -54,4 +59,29 @@ public class CounselServiceTest {
 
     }
 
+    @Test
+    void Should_ReturnResponseOfExistCounselEntity_When_RequestExistCounselId(){
+
+        Long findId = 1L;
+
+        Counsel entity = Counsel.builder()
+                .counselId(1L)
+                .build();
+
+        when(counselRepository.findById(findId)).thenReturn(Optional.ofNullable(entity));
+
+        CounselDTO.Response actual = counselService.get(findId);
+
+        assertThat(actual.getCounselId()).isSameAs(findId);
+    }
+    // 존재하지 않는 id에 대한 테스트
+    @Test
+    void Should_throwException_When_RequestNotExistCounselId(){
+
+        Long findId = 2L;
+
+        when(counselRepository.findById(findId)).thenThrow(new BaseException(ResultType.SYSTEM_ERROR));
+
+        Assertions.assertThrows(BaseException.class, () -> counselService.get(findId));
+    }
 }
