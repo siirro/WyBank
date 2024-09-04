484750af8fc554945ff7d9f85671ae4ea2a82bc4
dahee <dltmfekgml@naver.com>
2024-09-04 18:28:28 +0900

    feature: 대출상담 수정api

--- src/main/java/com/wybank/exception/ResultType.java
+++ src/main/java/com/wybank/exception/ResultType.java
@@ -9,7 +9,9 @@ public enum ResultType {
 
     SUCCESS("0000", "success"),
 
-    SYSTEM_ERROR("9000", "system error");
+    SYSTEM_ERROR("9000", "system error"),
+
+    ARTICLE_NOT_FOUND("0001", "article not found"),;
 
     private final String code;
     private final String desc;
